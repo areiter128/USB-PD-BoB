60dc4025b8a7bd66802e6e46b2ac2b0ec73ba3dd
M91406 <andreas.reiter@microchip.com>
2019-05-28 17:26:21 +0200

    Removed UART pin configuration from general init_gpio() function and moved it to task_UART.c initialization routine Signed-off-by:M91406 <andreas.reiter@microchip.com>

--- src/hal/initialization/init_gpio.c
+++ src/hal/initialization/init_gpio.c
@@ -28,20 +28,7 @@
 
 volatile uint16_t init_gpio(void) {
     
-    // UART1 for debug messages
-    UART_TX_INIT_OUTPUT;
-    UART_RX_INIT_INPUT;
-
-    // Set PPS for UART
-//    __builtin_write_RPCON(0x0000); // unlock PPS
-//    RPINR18bits.U1RXR = 0x003E;    //RC14->UART1:U1RX
-//    RPOR15bits.RP63R = 0x0001;    //RC15->UART1:U1TX
-//    __builtin_write_RPCON(0x0800); // lock PPS
-
-    pps_UnlockIO();
-    pps_RemapInput(UART_RX_RP, PPSIN_U1RX);
-    pps_RemapOutput(UART_TX_RP, PPSOUT_U1TX);
-    pps_LockIO();
-
+    Nop();
+    
     return(true);    
 }
