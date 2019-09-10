e4787d587eb9a7712f13ae50c2e36f578606dae6
M91406 <andreas.reiter@microchip.com>
2019-09-10 20:13:07 +0200

    Renamed the notify fault level status flag bit from NOTICE to NOTIFY

--- h/_root/generic/task_manager.h
+++ h/_root/generic/task_manager.h
@@ -138,7 +138,7 @@ typedef struct {
     volatile unsigned :1; // Bit #10: (reserved)
     volatile unsigned :1; // Bit #11: (reserved)
     volatile unsigned :1; // Bit #12: (reserved)
-	volatile bool global_notice:1;	// Bit #13: flag bit indicating the presence of notify events
+	volatile bool global_notify:1;	// Bit #13: flag bit indicating the presence of notify events
 	volatile bool global_warning:1;	// Bit #14: flag bit indicating the presence of warning events
 	volatile bool global_fault:1;	// Bit #15: flag bit indicating the presence of fault events
 } __attribute__((packed))TASK_MANAGER_STATUS_FLAGS_t;
