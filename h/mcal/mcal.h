492ea8e61e2981fa0a932ef213a36e6f2da5d2d6
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2020-01-28 20:10:15 -0700

    cleaned up header inclusion of MCAL plibrary

--- h/mcal/mcal.h
+++ h/mcal/mcal.h
@@ -71,17 +71,7 @@ extern "C" {
 #include "mcal/initialization/init_fosc.h"
 
 /* generic peripheral drives */    
-#include "dsPIC33C/p33SMPS_irq.h"
-#include "dsPIC33C/p33SMPS_dsp.h"
-#include "dsPIC33C/p33SMPS_hsadc.h"
-#include "dsPIC33C/p33SMPS_hspwm_c.h"
-#include "dsPIC33C/p33SMPS_gpio.h"
-#include "dsPIC33C/p33SMPS_oscillator.h"
-#include "dsPIC33C/p33SMPS_pmd.h"
-#include "dsPIC33C/p33SMPS_timer.h"
-#include "dsPIC33C/p33SMPS_pps.h"
-#include "dsPIC33C/p33SMPS_pmd.h"
-#include "dsPIC33C/p33SMPS_mailboxes.h"
+#include "../p33SMPS_plib.h"
 
 
 /* ***********************************************************************************************
